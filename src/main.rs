249af479406fd414e08550bf1e396225cb500b78
cyrus <cyrus.cryptor@gmail.com>
2025-09-24 15:05:39 +0800

    Use new method

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
-    let conf = parse_conf(&args);
+
+    // let conf  =  parse_conf(&args);
+    let conf =  Config::new(&args);
+
 
     println!("The query is: {}",conf.query);
     println!("The file path is: {}",conf.file_path);
@@ -19,15 +22,24 @@ struct Config {
     file_path: String,
 }
 
-fn parse_conf(args: &[String]) -> Config {
-
-    if args.len() <= 2 {
-        panic!("No enough args....")
-    }
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config {
-        query: query,
-        file_path: file_path,
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config { 
+            query: args[1].clone(),
+            file_path: args[2].clone() 
+        }
     }
 }
+
+// fn parse_conf(args: &[String]) -> Config {
+
+//     if args.len() <= 2 {
+//         panic!("No enough args....")
+//     }
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+//     Config {
+//         query: query,
+//         file_path: file_path,
+//     }
+// }
