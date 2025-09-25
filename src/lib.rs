1ea6e64526e7e15c3d2e0c69db2c447bcd9d4d57
cyrus <cyrus.cryptor@gmail.com>
2025-09-25 19:06:50 +0800

    add for ignore case

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,18 @@ pub fn search<'a>(query: &str, contents: &'a str ) -> Vec<&'a str> {
     res
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    let query = &query.to_lowercase();
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(query) {
+            res.push(line);
+        }
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -22,6 +34,31 @@ Rust:
 safe, fast, productiove.
 Pick there.";
         assert_eq!(vec!["safe, fast, productiove."], search(query, contents));
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:","Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 
 }
